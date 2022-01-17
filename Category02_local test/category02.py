800e826a03b67823ef29f532d20c8011cafd7b92
JH9892 <jhchoi09@naver.com>
2022-01-18 03:51:56 +0900

    upload local study in Category 02

--- Category02_local test/category02.py
+++ Category02_local test/category02.py
@@ -25,23 +25,60 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (x_train, y_train), (x_valid, y_valid) = fashion_mnist.load_data()
 
+# 정규화(Normalization)합니다.
 x_train = x_train / 255.0
 x_valid = x_valid / 255.0
 
-# 시각화
-fig, axes = plt.subplots(2, 5)
-fig.set_size_inches(10, 5)
-
-for i in range(10):
-    axes[i//5, i%5].imshow(x_train[i], cmap='gray')
-    axes[i//5, i%5].set_title(str(y_train[i]), fontsize=15)
-    plt.setp( axes[i//5, i%5].get_xticklabels(), visible=False)
-    plt.setp( axes[i//5, i%5].get_yticklabels(), visible=False)
-    axes[i//5, i%5].axis('off')
-
-plt.tight_layout()
-plt.show()
-
 tf.keras.backend.set_floatx('float64')
 x = Flatten(input_shape=(28, 28))
-print(x(x_train).shape)
+# print(x(x_train).shape)
+
+model = Sequential([
+    # Flatten으로 shape 펼치기
+    Flatten(input_shape=(28, 28)),
+    # Dense Layer
+    Dense(1024, activation='relu'),
+    Dense(512, activation='relu'),
+    Dense(256, activation='relu'),
+    Dense(128, activation='relu'),
+    Dense(64, activation='relu'),
+    # Classification을 위한 Softmax
+    Dense(10, activation='softmax'),
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['acc'])
+
+checkpoint_path = "my_checkpoint.ckpt"
+checkpoint = ModelCheckpoint(filepath=checkpoint_path,
+                             save_weights_only=True,
+                             save_best_only=True,
+                             monitor='val_loss',
+                             verbose=1)
+
+history = model.fit(x_train, y_train,
+                    validation_data=(x_valid, y_valid),
+                    epochs=20,
+                    callbacks=[checkpoint],
+                   )
+
+# checkpoint 를 저장한 파일명을 입력합니다.
+model.load_weights(checkpoint_path)
+model.evaluate(x_valid, y_valid)
+
+plt.figure(figsize=(12, 9))
+plt.plot(np.arange(1, 21), history.history['loss'])
+plt.plot(np.arange(1, 21), history.history['val_loss'])
+plt.title('Loss / Val Loss', fontsize=20)
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend(['loss', 'val_loss'], fontsize=15)
+plt.show()
+
+plt.figure(figsize=(12, 9))
+plt.plot(np.arange(1, 21), history.history['acc'])
+plt.plot(np.arange(1, 21), history.history['val_acc'])
+plt.title('Acc / Val Acc', fontsize=20)
+plt.xlabel('Epochs')
+plt.ylabel('Acc')
+plt.legend(['acc', 'val_acc'], fontsize=15)
+plt.show()
